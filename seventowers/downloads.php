a4cc3d3192ad4a88c384828628f16f641ce82358
Marzo Sette Torres Junior <marzojr@yahoo.com>
2016-03-13 19:57:33 -0300

    Fixing array key check.

--- seventowers/downloads.php
+++ seventowers/downloads.php
@@ -33,7 +33,7 @@
 	if( isset($_REQUEST["section"]) && is_numeric($_REQUEST["section"]) )
 		$section = intval($_REQUEST["section"]);
 	else
-		$section = 0;
+		$section = 1;
 
 	// Silently correct errors.
 	if( isset($_REQUEST["page"]) && is_numeric($_REQUEST["page"]) )
