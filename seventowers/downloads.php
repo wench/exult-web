7ab0dd593d97d2d4f35941802fc7a768de2ed7e3
Marzo Sette Torres Junior <marzojr@yahoo.com>
2016-03-13 19:15:23 -0300

    Changing scripts so the dirs are actually merged.

--- seventowers/downloads.php
+++ seventowers/downloads.php
@@ -1,33 +1,33 @@
 <?php
 	$PAGES = array(
-    	array(
-    		"DAT"	=>	"exultmods.dat",
-    		"IMG"	=>	"exultmodstitle.png",
-      		"HEADS"	=>	array(
-        		"Exult Mods",
-        		"Black Gate Keyring",
-        		"Serpent Isle Fixes",
-        		"Avatar Pack",
-    		)
+		array(
+			"DAT"	=>	"exultmods.dat",
+			"IMG"	=>	"exultmodstitle.png",
+			"HEADS"	=>	array(
+				"Exult Mods",
+				"Black Gate Keyring",
+				"Serpent Isle Fixes",
+				"Avatar Pack",
+			)
 		),
 		array(
-    		"DAT"	=>	"usecode.dat",
-    		"IMG"	=>	"usecodetitle.png",
-    		"HEADS"	=>	array(
-        		"Usecode C",
-        		"Tutorials",
-        		"Reference",
-        		"Syntax Highlighters",
-    		)
+			"DAT"	=>	"usecode.dat",
+			"IMG"	=>	"usecodetitle.png",
+			"HEADS"	=>	array(
+				"Usecode C",
+				"Tutorials",
+				"Reference",
+				"Syntax Highlighters",
+			)
 		),
 		array(
-    		"DAT"	=>	"ultimaicons.dat",
-    		"IMG"	=>	"ultimaicons.png",
-    		"HEADS"	=>	array(
-        		"Ultima Icons"
-    		)
+			"DAT"	=>	"ultimaicons.dat",
+			"IMG"	=>	"ultimaicons.png",
+			"HEADS"	=>	array(
+				"Ultima Icons"
+			)
 		),
-  );
+	);
 
 	// Silently correct errors.
 	if( isset($_REQUEST["section"]) && is_numeric($_REQUEST["section"]) )
