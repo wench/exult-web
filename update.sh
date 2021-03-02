3561828f03790caba5f1133ba04dcb781ab972ff
Marzo Sette Torres Junior <marzojr@gmail.com>
2021-03-02 23:36:24 +0100

    Fixing update script

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [! -f master.zip ]; then
+if [ ! -f master.zip ]; then
     echo "master.zip not found; perhaps you forgot to scp it?"
     exit 1
 fi
 
-unzip -fo master.zip
+unzip -uo master.zip
 rm -f master.zip
 chgrp -R exult htdocs/
 chmod -f -R a+r htdocs/
