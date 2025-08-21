91354a98af89e3100a3e8f8eb00a81b0c7379b88
Alexis Ryan <the.wench@wenchy.net>
2025-08-21 22:52:47 +1000

    Workflowtest

--- gen-snapshot-file-details.sh
+++ gen-snapshot-file-details.sh
@@ -3,7 +3,7 @@
 echo "var static_snapshot_file_details = {" |tee shapshot_file_details.js
 grep -o "https://exult.sourceforge.io/snapshots/[^\"]*" download.html | while read p; do
 echo "  '$p':  {"|tee -a shapshot_file_details.js
-  curl --silent --show-error --head "$p"  | sed -r -n "s/(content-length|last-modified): +(.*)/   '\1': '\2',/p" | tee -a shapshot_file_details.js
+  curl --silent --show-error --head "$p" | sed -r -n "s/(content-length|last-modified): +(.*)/   '\1': '\2',/p" | tee -a shapshot_file_details.js
 echo "  }," | tee -a shapshot_file_details.js
   done
 echo "};" |tee -a shapshot_file_details.js
